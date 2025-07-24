d02b521edaba6fb2813d7d299d67144e243b0b89
Morgan Hubbell <morgan.hubbell@gmail.com>
2025-07-23 20:19:21 -0400

    added feeder subsytem and commands A

---
+++ src/main/java/frc/robot/subsystems/FeederSubsystem.kt
@@ -0,0 +1,28 @@
+package frc.robot.subsystems
+
+import com.ctre.phoenix6.controls.DutyCycleOut
+import com.ctre.phoenix6.hardware.TalonFX
+import com.ctre.phoenix6.signals.ForwardLimitValue
+import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+
+object FeederSubsystem : SubsystemBase() {
+    private val motor = TalonFX(15)
+
+    fun Intake(): Command {
+        return startEnd(
+                { motor.setControl(intakeControl.withOutput(0.5)) },
+                { motor.setControl(intakeControl.withOutput(0.0)) },
+            )
+            .until { motor.forwardLimit.value == ForwardLimitValue.ClosedToGround }
+    }
+    fun Shooter(): Command {
+        return startEnd(
+            { motor.setControl(shooterControl.withOutput(0.5)) },
+            { motor.setControl(shooterControl.withOutput(0.0)) },
+        )
+            .until { motor.forwardLimit.value == ForwardLimitValue.Open }
+    }
+    val intakeControl = DutyCycleOut(0.0).withIgnoreHardwareLimits(false)
+    val shooterControl = DutyCycleOut(0.0).withIgnoreHardwareLimits(true)
+}
